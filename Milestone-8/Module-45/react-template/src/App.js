1a5bfed9bcbfb0bd6c8f7475f9b227b5b91500b2
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-11-03 21:33:45 +0600

    Milestone-8

--- Milestone-8/Module-45/react-template/src/App.js
+++ Milestone-8/Module-45/react-template/src/App.js
@@ -3,7 +3,54 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Hello React JS</h1>
+      <div className='container'>
+        <h1>React.JS</h1>
+        <p>A JavaScript library for building user interfaces</p>
+      </div>
+      <Tools />
+      <FrontEnd Language='JavaScript' framework='React.JS' />
+      <BackEnd server='Node.JS' database='MongoDB' />
+    </div>
+  );
+}
+
+function Tools() {
+  const tools = ['VS Code', 'Git & GitHub'];
+  return (
+    <div className='tools'>
+      {tools.map((tool) => (
+        <ul>
+          <li>{tool}</li>
+        </ul>
+      ))}
+    </div>
+  );
+}
+
+const FrontEnd = ({ Language, framework }) => {
+  return (
+    <div className='front-end'>
+      <h2>Front-End Developer</h2>
+      <ol>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>{Language}</li>
+        <li>{framework}</li>
+      </ol>
+    </div>
+  );
+};
+
+function BackEnd(props) {
+  return (
+    <div className='back-end'>
+      <h2>Back-End Developer</h2>
+      <ol>
+        <li>Firebase</li>
+        <li>{props.server}</li>
+        <li>Express.JS</li>
+        <li>{props.database}</li>
+      </ol>
     </div>
   );
 }
