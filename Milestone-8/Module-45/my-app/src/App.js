9496ebef478792ebe3e814099456c2c24bb71f5e
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-11-03 23:28:10 +0600

    Milestone-8

---
+++ Milestone-8/Module-45/my-app/src/App.js
@@ -0,0 +1,60 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+function App() {
+  return (
+    <div className='App'>
+      <Counter />
+      <ExternalUsers />
+    </div>
+  );
+}
+// Counter Function
+function Counter() {
+  // State
+  const [count, setCount] = useState(0);
+  // Decrement onClick Function
+  const decrement = () => {
+    const newCount = count - 1;
+    setCount(newCount);
+  };
+  // Increment onClick Function
+  const increment = () => setCount(count + 1);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={decrement}> - </button>
+      <button onClick={increment}> + </button>
+    </div>
+  );
+}
+
+const ExternalUsers = () => {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((res) => res.json())
+      .then((data) => setUsers(data));
+  }, []);
+  return (
+    <div className='users-container'>
+      <h1>External Users</h1>
+      <p>{users.length}</p>
+      {users.map((user) => (
+        <User id={user.id} name={user.name} email={user.email} />
+      ))}
+    </div>
+  );
+};
+
+function User(props) {
+  return (
+    <div className='user-container'>
+      <h3>{props.id}</h3>
+      <h4>{props.name}</h4>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+export default App;
