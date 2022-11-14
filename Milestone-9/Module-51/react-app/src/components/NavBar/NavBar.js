e291d51988151fa7ef1c381317421d61c40a943b
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2022-11-14 22:12:58 +0600

    Milestone-9

---
+++ Milestone-9/Module-51/react-app/src/components/NavBar/NavBar.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Link from '../Link/Link';
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/solid';
+
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+  const routes = [
+    { id: 1, name: 'Home', path: '/home' },
+    { id: 2, name: 'Products', path: '/products' },
+    { id: 3, name: 'Orders', path: '/orders' },
+    { id: 4, name: 'Contact', path: '/contact' },
+    { id: 5, name: 'About', path: '/about' },
+  ];
+  return (
+    <nav>
+      {open ? (
+        <XMarkIcon onClick={() => setOpen(!open)} className='h-6 w-6' />
+      ) : (
+        <Bars3Icon onClick={() => setOpen(!open)} className='h-6 w-6' />
+      )}
+      <span>{open ? 'Open' : 'Close'}</span>
+
+      <ul className='md:flex justify-center '>
+        {routes.map((route) => (
+          <Link key={route.id} route={route} />
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
