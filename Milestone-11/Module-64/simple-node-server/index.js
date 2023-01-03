dc4aec093b56ff0db1134218a532dee0ea43bc17
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-01-03 18:31:05 +0600

    Milestone-11

--- Milestone-11/Module-64/simple-node-server/index.js
+++ Milestone-11/Module-64/simple-node-server/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 5000;
 
 const users = [
   {
@@ -13,13 +14,13 @@ const users = [
   },
   {
     id: 2,
-    name: 'Marufa',
-    email: 'marufa@gmail.com',
+    name: 'Tasin',
+    email: 'tasin@gmail.com',
   },
   {
     id: 3,
-    name: 'Umme Salma',
-    email: 'umme@gmail.com',
+    name: 'Tahmid',
+    email: 'tahmid@gmail.com',
   },
 ];
 
