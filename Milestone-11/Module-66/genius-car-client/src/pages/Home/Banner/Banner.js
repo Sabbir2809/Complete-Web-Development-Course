3c750b1743b99a06aeb4140ecf6b6240e6a64914
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-01-20 10:32:11 +0600

    Milestone-11

--- Milestone-11/Module-66/genius-car-client/src/pages/Home/Banner/Banner.js
+++ Milestone-11/Module-66/genius-car-client/src/pages/Home/Banner/Banner.js
@@ -1,58 +1,58 @@
 import React from 'react';
-import bannar1 from './../../../assets/images/banner/1.jpg';
-import bannar2 from './../../../assets/images/banner/2.jpg';
-import bannar3 from './../../../assets/images/banner/3.jpg';
-import bannar4 from './../../../assets/images/banner/4.jpg';
-import bannar5 from './../../../assets/images/banner/5.jpg';
-import bannar6 from './../../../assets/images/banner/6.jpg';
+import banner1 from './../../../assets/images/banner/1.jpg';
+import banner2 from './../../../assets/images/banner/2.jpg';
+import banner3 from './../../../assets/images/banner/3.jpg';
+import banner4 from './../../../assets/images/banner/4.jpg';
+import banner5 from './../../../assets/images/banner/5.jpg';
+import banner6 from './../../../assets/images/banner/6.jpg';
+import BannerItem from './BannerItem';
+
+const bannerData = [
+  {
+    image: banner1,
+    prev: 6,
+    id: 1,
+    next: 2,
+  },
+  {
+    image: banner2,
+    prev: 1,
+    id: 2,
+    next: 3,
+  },
+  {
+    image: banner3,
+    prev: 2,
+    id: 3,
+    next: 4,
+  },
+  {
+    image: banner4,
+    prev: 3,
+    id: 4,
+    next: 5,
+  },
+  {
+    image: banner5,
+    prev: 4,
+    id: 5,
+    next: 6,
+  },
+  {
+    image: banner6,
+    prev: 5,
+    id: 6,
+    next: 1,
+  },
+];
 
 const Banner = () => {
   return (
-    <div className='carousel w-full'>
-      <div id='slide1' className='carousel-item relative w-full'>
-        <img src={bannar1} className='w-full' alt='banner1' />
-        <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-          <a href='#slide4' className='btn btn-circle'>
-            ❮
-          </a>
-          <a href='#slide2' className='btn btn-circle'>
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id='slide2' className='carousel-item relative w-full'>
-        <img src={bannar2} className='w-full' alt='banner2' />
-        <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-          <a href='#slide1' className='btn btn-circle'>
-            ❮
-          </a>
-          <a href='#slide3' className='btn btn-circle'>
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id='slide3' className='carousel-item relative w-full'>
-        <img src={bannar3} className='w-full' alt='banner3' />
-        <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-          <a href='#slide2' className='btn btn-circle'>
-            ❮
-          </a>
-          <a href='#slide4' className='btn btn-circle'>
-            ❯
-          </a>
-        </div>
-      </div>
-      <div id='slide4' className='carousel-item relative w-full'>
-        <img src={bannar4} className='w-full' alt='banner4' />
-        <div className='absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2'>
-          <a href='#slide3' className='btn btn-circle'>
-            ❮
-          </a>
-          <a href='#slide1' className='btn btn-circle'>
-            ❯
-          </a>
-        </div>
-      </div>
+    <div className='carousel w-full py-10'>
+      {/* <BannerItem image={banner1}></BannerItem> */}
+      {bannerData.map((slide) => (
+        <BannerItem key={bannerData.id} slide={slide}></BannerItem>
+      ))}
     </div>
   );
 };
