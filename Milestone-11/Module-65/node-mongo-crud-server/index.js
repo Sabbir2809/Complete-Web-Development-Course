a37cfde9b79c9a5f31a4d7fbae93d3dd5c3ce6a0
Sabbir2809 <sabbir15-2809@diu.edu.bd>
2023-01-21 13:41:26 +0600

    Milestone-11

--- Milestone-11/Module-65/node-mongo-crud-server/index.js
+++ Milestone-11/Module-65/node-mongo-crud-server/index.js
@@ -2,14 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, Collection, ObjectId } = require('mongodb');
 const app = express();
+require('dotenv').config();
 // port
 const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
 
-const uri =
-  'mongodb+srv://node-mongo:adYmchbLah3S6B7E@cluster0.an7cx.mongodb.net/?retryWrites=true&w=majority';
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.an7cx.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
